53022896c6f7c90e2065ee69cf326e5b23317709
N/A <N/A>
2022-08-30 21:30:57 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,142 @@
+use {
+    core::ops::{AddAssign, DivAssign, MulAssign, SubAssign},
+    num_traits::{FromPrimitive, PrimInt, ToPrimitive, WrappingAdd, WrappingMul, WrappingSub},
+    std::num::{NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize},
+};
+
+/// An unsigned integer.
+/// `u8`, `u16`, `u32`, `u64`, `usize`.
+pub trait Unsigned:
+    PrimInt
+    + num_traits::Unsigned
+    + AddAssign
+    + SubAssign
+    + MulAssign
+    + DivAssign
+    + WrappingAdd
+    + WrappingSub
+    + WrappingMul
+    + FromPrimitive
+    + ToPrimitive
+{
+    /// [`FromPrimitive::from_u8`] succeeds for all supported types.
+    fn from_u8(byte: u8) -> Self;
+
+    /// [`ToPrimitive::to_usize`] succeeds for all supported types,
+    /// except for `u32` and `u64` on 16-bit platforms and
+    /// `u64` on 32-bit platforms.
+    fn to_usize(self) -> usize;
+}
+
+impl Unsigned for u8 {
+    fn from_u8(byte: u8) -> Self {
+        byte as _
+    }
+
+    fn to_usize(self) -> usize {
+        self as _
+    }
+}
+
+impl Unsigned for u16 {
+    fn from_u8(byte: u8) -> Self {
+        byte as _
+    }
+
+    fn to_usize(self) -> usize {
+        self as _
+    }
+}
+
+impl Unsigned for u32 {
+    fn from_u8(byte: u8) -> Self {
+        byte as _
+    }
+
+    fn to_usize(self) -> usize {
+        // May fail on a 16-bit platform.
+        // Don't really care about those, but still leaving the `expect()` just in case.
+        <Self as ToPrimitive>::to_usize(&self)
+            .expect("tried to convert `u32` to `usize` with overflow")
+    }
+}
+
+impl Unsigned for u64 {
+    fn from_u8(byte: u8) -> Self {
+        byte as _
+    }
+
+    fn to_usize(self) -> usize {
+        // May fail on a 32-bit platform.
+        // Don't really care about those, but still leaving the `expect()` just in case.
+        <Self as ToPrimitive>::to_usize(&self)
+            .expect("tried to convert `u64` to `usize` with overflow")
+    }
+}
+
+impl Unsigned for usize {
+    fn from_u8(byte: u8) -> Self {
+        byte as _
+    }
+
+    fn to_usize(self) -> usize {
+        self
+    }
+}
+
+/// A non-zero unsigned integer.
+/// `NonZeroU8`, `NonZeroU16`, `NonZeroU32`, `NonZeroU64`, `NonZeroUsize`.
+pub trait NonZero<U: Unsigned>: Copy {
+    fn new(val: U) -> Option<Self>;
+    fn get(self) -> U;
+}
+
+impl NonZero<u8> for NonZeroU8 {
+    fn new(val: u8) -> Option<Self> {
+        Self::new(val)
+    }
+
+    fn get(self) -> u8 {
+        self.get()
+    }
+}
+
+impl NonZero<u16> for NonZeroU16 {
+    fn new(val: u16) -> Option<Self> {
+        Self::new(val)
+    }
+
+    fn get(self) -> u16 {
+        self.get()
+    }
+}
+
+impl NonZero<u32> for NonZeroU32 {
+    fn new(val: u32) -> Option<Self> {
+        Self::new(val)
+    }
+
+    fn get(self) -> u32 {
+        self.get()
+    }
+}
+
+impl NonZero<u64> for NonZeroU64 {
+    fn new(val: u64) -> Option<Self> {
+        Self::new(val)
+    }
+
+    fn get(self) -> u64 {
+        self.get()
+    }
+}
+
+impl NonZero<usize> for NonZeroUsize {
+    fn new(val: usize) -> Option<Self> {
+        Self::new(val)
+    }
+
+    fn get(self) -> usize {
+        self.get()
+    }
+}
